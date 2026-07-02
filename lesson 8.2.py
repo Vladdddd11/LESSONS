f1acdf9bdd41a4e30c118fc43848cd20c57abe78
Vladislav <Vladislavdrachuk48@gmail.com>
2026-07-03 00:19:12 +0300

    Home_work lesson 8/2

---
+++ lesson 8.2.py
@@ -0,0 +1,23 @@
+# Ваше завдання – написати функцію is_palindrome, яка перевірятиме, чи є рядок паліндромом.
+# Паліндромом - це такий рядок, який читається однаково зліва направо і
+# зправа наліво без урахування знаків пунктуації та розмірності букв.
+# Функція приймає на вхід рядок, та повертає булеве значення True або False
+# Приклад:
+import string
+
+
+
+def is_palindrome(text):
+
+    examination = string.punctuation
+    for symbol in examination:
+        text = text.replace(symbol, "")
+    text = text.lower()
+    text = text.replace(" ", "")
+    return text==text[::-1]
+
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+print("ОК")
